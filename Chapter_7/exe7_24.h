591fc58af55e255c2e6c19daf13eb5bfbf0ea92b
SneakerMa <16568605+SneakerMa@users.noreply.github.com>
2020-04-12 20:44:25 +0800

    C++ Primer Chapter 7

---
+++ Chapter_7/exe7_24.h
@@ -0,0 +1,21 @@
+#include <string>
+
+class Screen{
+public:
+    typedef std::string::size_type pos;
+    Screen() = default;
+    Screen(pos ht, pos wd): height(ht), width(wd), contents(ht * wd, ' ') {}
+    Screen(pos ht, pos wd, char c): height(ht), width(wd), contents(ht * wd, c) {}
+    
+    char get() const { return contents[cursor]; }
+    char get(pos r, pos c) const
+    {
+        pos row = r * width;
+        return contents[row + c];
+    }
+
+private:
+    pos cursor = 0;
+    pos height = 0, width = 0;
+    std::string contents;
+};
---
+++ Chapter_7/exe7_41.h
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+
+class Sales_data {
+    friend std::istream &read(std::istream &, Sales_data &);
+    friend std::ostream &print(std::ostream &, const Sales_data &);
+    friend Sales_data add(const Sales_data &, const Sales_data &);
+
+private:
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+
+public:
+    Sales_data(const std::string &s, unsigned n, double p)
+            : bookNo(s), units_sold(n), revenue(p*n) 
+    {
+        std::cout << "Sales_data(const std::string &s, unsigned n, double p)" 
+                << std::endl;
+    }
+
+    Sales_data(): Sales_data("", 0, 0)
+    {
+        std::cout << "Sales_data()" << std::endl;
+    }
+
+    Sales_data(const std::string &s): Sales_data(s, 0, 0.0)
+    {
+        std::cout << "Sales_data(const std::string &s)" << std::endl;
+    }
+
+    Sales_data(std::istream &is): Sales_data() 
+    { 
+        std::cout << "Sales_data(std::istream &is)" << std::endl;
+        read(is, *this); 
+    }
+
+    std::string isbn() const { return bookNo; }
+    Sales_data &combine(const Sales_data &);
+    double avg_price() const;
+};
+
+// member functions.
+inline double Sales_data::avg_price() const
+{
+    return units_sold ? revenue / units_sold : 0;
+}
+
+Sales_data& Sales_data::combine(const Sales_data &rhs)
+{
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+// nonmember functions
+std::istream &read(std::istream& is, Sales_data& item)
+{
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = price * item.units_sold;
+    return is;
+}
+
+std::ostream &print(std::ostream& os, const Sales_data& item)
+{
+    os << item.isbn() << " " << item.units_sold << " " << item.revenue;
+    return os;
+}
+
+Sales_data add(const Sales_data& lhs, const Sales_data& rhs)
+{
+    Sales_data sum = lhs;
+    sum.combine(rhs);
+    return sum;
+}
